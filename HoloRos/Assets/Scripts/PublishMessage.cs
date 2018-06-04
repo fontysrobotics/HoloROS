d7c0c7fbffbb0c1e96546eb8c4de0644ced07ebf
Jasper de Weger <jdeweger@live.com>
2018-06-04 14:04:27 +0200

    Inserted first good program

---
+++ HoloRos/Assets/Scripts/PublishMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RosTypes
+{
+    class PublishMessage
+    {
+        public string op;
+        public string topic;
+        public RosMsg msg;
+
+        public PublishMessage(string op, string topic, RosMsg msg)
+        {
+            this.op = op;
+            this.topic = topic;
+            this.msg = msg;
+
+        }
+
+    }
+}
---
+++ HoloRos/Assets/Scripts/geometry_msgs_twist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RosTypes
+{
+    class geometry_msgs_twist : RosMsg
+    {
+        public geometry_msgs_vector3 linear;
+        public geometry_msgs_vector3 angular;
+    }
+}
